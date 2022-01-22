3fe62eb6f115e209219336b5dd641a13f1068500
shiguma127 <shiguma127@gmail.com>
2022-01-23 00:21:01 +0900

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::{collections::HashMap};
+
+use chrono::Utc;
+use http::StatusCode;
+use worker::*;
+use sha256::{digest};
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env) -> Result<Response> {
+    log_request(&req);
+    utils::set_panic_hook();
+    let router = Router::new();
+    router
+        .get("/", |req, _| {
+            let query_pairs: HashMap<_, _> = req.url()?.query_pairs().into_owned().collect();
+            let user_answer = match query_pairs.get("answer") {
+                Some(answer) => answer,
+                None => return Response::error("Batrequest", StatusCode::BAD_REQUEST.as_u16()),
+            };
+            let user_answer:bool = match user_answer.parse() {
+                Ok(answer) => answer,
+                Err(_) => return Response::error("Batrequest", StatusCode::BAD_REQUEST.as_u16()),
+            };
+            let utc_date: chrono::Date<Utc> = Utc::today();
+            let hash = digest(format!{"{}",utc_date});
+            let bytes = hash.as_bytes();
+            let bit = bytes[0] >> 7;
+            let answer = bit & 0x00000001 > 0;
+            let res = answer == user_answer;
+            let mut response  = Response::from_json(&res).unwrap();
+            response.headers_mut().append("Access-Control-Allow-Origin", "http://localhost:3000")?;
+            response.headers_mut().append("Access-Control-Allow-Methods","GET")?;
+            response.headers_mut()
+        .append("access-control-allow-methods","GET")?;
+            Ok(response)
+        })
+        .run(req, env)
+        .await
+}
